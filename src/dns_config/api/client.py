8b2f0a36ec333bf7232e2a95a7f26861356ce5a8
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2024-11-15 15:26:05 +0530

    added sample program

--- src/dns_config/api/client.py
+++ src/dns_config/api/client.py
@@ -1,22 +1,21 @@
 from bloxone_client import ApiClient
-from dns_config.api import AclApi , AuthNsgApi , AuthZoneApi , CacheFlushApi , ConvertDomainNameApi , ConvertRnameApi , DelegationApi , ForwardNsgApi , ForwardZoneApi , GlobalApi , HostApi , LbdnApi , ServerApi , ViewApi
-
+import dns_config.api as api
 
 
 class NewApiClient :
     def __init__(self):
         self.APIClient = ApiClient.new_api_client(self)
-        self.acl_api = AclApi(self.APIClient)
-        self.auth_nsg_api = AuthNsgApi(self.APIClient)
-        self.auth_zone_api = AuthZoneApi(self.APIClient)
-        self.cache_flush_api = CacheFlushApi(self.APIClient)
-        self.convert_domain_name_api = ConvertDomainNameApi(self.APIClient)
-        self.convert_rname_api = ConvertRnameApi(self.APIClient)
-        self.delegation_api = DelegationApi(self.APIClient)
-        self.forward_nsg_api = ForwardNsgApi()
-        self.forward_zone_api = ForwardZoneApi(self.APIClient)
-        self.global_api = GlobalApi(self.APIClient)
-        self.host_api = HostApi(self.APIClient)
-        self.lbdn_api = LbdnApi(self.APIClient)
-        self.server_api = ServerApi(self.APIClient)
-        self.view_api = ViewApi(self.APIClient)
+        self.acl_api = api.AclApi(self.APIClient)
+        self.auth_nsg_api = api.AuthNsgApi(self.APIClient)
+        self.auth_zone_api = api.AuthZoneApi(self.APIClient)
+        self.cache_flush_api = api.CacheFlushApi(self.APIClient)
+        self.convert_domain_name_api = api.ConvertDomainNameApi(self.APIClient)
+        self.convert_rname_api = api.ConvertRnameApi(self.APIClient)
+        self.delegation_api = api.DelegationApi(self.APIClient)
+        self.forward_nsg_api = api.ForwardNsgApi()
+        self.forward_zone_api = api.ForwardZoneApi(self.APIClient)
+        self.global_api = api.GlobalApi(self.APIClient)
+        self.host_api = api.HostApi(self.APIClient)
+        self.lbdn_api = api.LbdnApi(self.APIClient)
+        self.server_api = api.ServerApi(self.APIClient)
+        self.view_api = api.ViewApi(self.APIClient)
