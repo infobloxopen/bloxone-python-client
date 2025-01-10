498add0e5c57d7cbad3391ef80417561534831a9
VishrutiBuddhadev <vbuddhadev@infoblox.com>
2025-01-10 14:21:36 +0530

    addressed review comments

--- src/ipam/models/address.py
+++ src/ipam/models/address.py
@@ -30,9 +30,7 @@ class Address(BaseModel):
     An __Address__ object (_ipam/address_) represents any single IP address within a given IP space.
     """ # noqa: E501
     address: Optional[StrictStr] = Field(
-        default=None,
-        description="The address in form \"a.b.c.d\"."
-    )
+        default=None, description="The address in form \"a.b.c.d\".")
     comment: Optional[StrictStr] = Field(
         default=None,
         description=
